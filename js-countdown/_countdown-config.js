d519488e6212d4b08c901532076873d867930619
gutenachtschicht <90715870+gutenachtschicht@users.noreply.github.com>
2022-01-28 19:41:54 +0100

    initial commit of yet another countdown

---
+++ js-countdown/_countdown-config.js
@@ -0,0 +1,49 @@
+
+// Set the date we're counting down to
+var countdownto = "June 30, 2087 00:00:00";
+// var countdownto = "Januar 28, 2020 00:00:00";
+
+// ---------------------- pictures ------------------
+
+// Set the picture you want to show in index.html
+var indexpic = "portrait.gif";
+// Set the picture you want to show in all.html
+var allpic = "portrait.gif";
+
+
+// ---------------------- strings ------------------
+
+// Give the text above the countdown in index.html
+var indexhead = "Only";
+// Give the text below the countdown in index.html
+var indexfoot = " to go for your retirement !";
+
+// Give the text between the coufull line and the list with all units 
+var alltext= " is the time you have to wait for retirement, or ";
+
+// Diplay text when countdown is over
+var countdownovertime = "... time is over ...";
+var countdownovertext = "Congrats for reaching retirement!";
+
+
+// ---------------------- list of units ------------------
+
+// strings for the composed line in all.html
+var stringyears    = " years ";
+var stringdays     = " days and ";
+var stringhours    = " hours and ";
+var stringminutes  = " minutes and ";
+var stringseconds  = " seconds";
+
+// time units used
+var unitsec = "seconds";
+var unitmin = "minutes";
+var unithrs = "hours";
+var unitdys = "days";
+var unitwks = "weeks";
+var unitmth = "months";
+var unityrs = "years";
+           
+
+
+// https://www.deutsche-rentenversicherung.de/DRV/DE/Online-Dienste/Online-Rechner/RentenbeginnUndHoehenRechner/rentenbeginnrechner_node.html
